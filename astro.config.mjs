b7817238bd18d2825c3fc4f01b33048eccef3b77
Marcus Rådell <marcus@radell.net>
2026-09-15 20:34:18 +0200

    update github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Bro Skatepark',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/marcusradell/bro-skatepark' }],
 			sidebar: [
 				{
 					label: 'Resources',
