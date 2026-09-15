51dc74344ce29aae7e0df2a88277152f78e4bb44
Marcus Rådell <marcus@radell.net>
2026-09-15 20:05:56 +0200

    setup basics with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,12 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Pages',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'Resources',
+					items: [{ autogenerate: { directory: 'resources' } }],
 				},
 			],
 		}),
