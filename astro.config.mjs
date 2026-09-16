ba7ba3e757b34041dbc866d397014f3bc793975e
Marcus Rådell <marcus@radell.net>
2026-09-16 21:47:28 +0200

    update deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://marcusradell.github.io/bro-skatepark",
 	integrations: [
 		starlight({
 			title: 'Bro Skatepark',
